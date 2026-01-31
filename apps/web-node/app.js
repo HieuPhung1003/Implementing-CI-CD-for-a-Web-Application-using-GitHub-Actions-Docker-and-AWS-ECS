b8059643d077bb45230f61641f3de626407f8189
HieuPhung1003 <phungminhhieu103204@gmail.com>
2026-01-31 14:47:28 +0700

    test another apps on port 80

--- apps/web-node/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 80;
-
-app.get('/', (req, res) => res.send('Hello World from CI/CD pipeline!'));
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
